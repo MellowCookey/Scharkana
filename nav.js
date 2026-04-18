a8ec795aa5578c9ce4e6d0cf612fdb59423ce308
MellowCookey <goodgoosegames@mailbox.org>
2026-04-18 19:41:03 +0200

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var page = location.pathname.split('/').pop().replace('.html', '') || 'index';
-  document.querySelectorAll('a.nav-btn[data-page]').forEach(function (a) {
-    if (a.dataset.page === page || (page === 'index' && a.dataset.page === 'willkommen')) {
-      a.classList.add('active');
-    }
-  });
-})();
