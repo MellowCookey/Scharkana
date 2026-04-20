16c933ebaf69860b1d2e803643483052aef8273b
MellowCookey <goodgoosegames@mailbox.org>
2026-04-20 12:37:49 +0200

    Add files via upload

--- nav.js
+++ nav.js
@@ -5,4 +5,21 @@
       a.classList.add('active');
     }
   });
+
+  var hamburger = document.querySelector('.nav-hamburger');
+  var navLinks  = document.querySelector('.nav-links');
+  if (hamburger && navLinks) {
+    hamburger.addEventListener('click', function () {
+      var open = navLinks.classList.toggle('open');
+      hamburger.classList.toggle('open', open);
+      hamburger.setAttribute('aria-expanded', open);
+    });
+    navLinks.querySelectorAll('a').forEach(function (a) {
+      a.addEventListener('click', function () {
+        navLinks.classList.remove('open');
+        hamburger.classList.remove('open');
+        hamburger.setAttribute('aria-expanded', 'false');
+      });
+    });
+  }
 })();
